b19f82e795f027f2efdcf1363f23b3c52e55f826
Jordan Cotton <Jordancotton55@gmail.com>
2017-05-29 09:39:51 +0300

    Created reuseable Button component

--- src/App.js
+++ src/App.js
@@ -119,15 +119,34 @@ class Table extends Component {
           <span>{item.num_components}</span>
           <span>{item.points}</span>
           <span>
-            <button
-            onClick={() => onDismiss(item.objectID)}
-            type="button"
-            > Dismiss
-            </button>
+            <Button 
+            onClick={() => onDismiss(item.objectID)}>
+            Dismiss 
+            </Button>
           </span>
         </div>
         )}
       </div>
+    );
+  }
+}
+
+class Button extends Component {
+  render() {
+    const { 
+      onClick,
+      className,
+      children
+    } = this.props;
+
+    return (
+      <button
+      onClick={onClick}
+      className={className}
+      type="button"
+      >
+       {children}
+       </button>
     );
   }
 }
