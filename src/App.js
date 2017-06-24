df004a2a1b84ae5de1a1516c743dc466e609a304
Jordan Cotton <Jordancotton55@gmail.com>
2017-06-24 15:50:57 +0300

    Update Search component into a functional stateless component

--- src/App.js
+++ src/App.js
@@ -90,20 +90,14 @@ class App extends Component {
   }
 }
 
-class Search extends Component {
-  render() {
-    const { value, onChange, children } = this.props; //Getting these from parent component, passed on the tag
-    return (
-      <form>
-        {children} <input //Uses children to display elements passed from the parent component
-        type="text"
-        value={value}
-        onChange={onChange}
-        />
-      </form>
-    );
-  }
-}
+const Search = ({value, onChange, children}) => 
+  <form>
+    {children} <input 
+    type="text"
+    value={value}
+    onChange={onChange}
+    />
+  </form>
 
 class Table extends Component {
   render() {
