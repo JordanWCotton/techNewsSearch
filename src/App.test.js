59a92800a9287dd39e39ec5c205214c3a5e65c09
Jordan Cotton <Jordancotton55@gmail.com>
2017-06-28 16:38:01 +0300

    Add react-test-render for Snapshot testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
 
-it('renders without crashing', () => {
+describe('App', () => {
+
+  it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+  });
+
+  test('snapshots', () => {
+    const component = renderer.create(
+      <App />
+    );
+
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
 });
+
+
