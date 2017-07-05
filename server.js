e077eb0e94e1d3c6072251b180e534e69d6d43fd
Jordan Cotton <Jordancotton55@gmail.com>
2017-07-05 17:32:33 +0300

    Fix typo in response

--- server.js
+++ server.js
@@ -7,5 +7,5 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/src/public.html'));
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
